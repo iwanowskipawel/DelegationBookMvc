4fffb79d94dbd64f080c7d318f29e092010ca4e2
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-02-09 14:34:39 +0100

    Add TripViewModel

---
+++ DelegationBook/Views/ViewModels/TripViewModel.cs
@@ -0,0 +1,16 @@
+﻿using DelegationBook.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DelegationBook.Views.ViewModels
+{
+    public class TripViewModel
+    {
+        public Trip Trip { get; set; }
+        public Employee SelectedEmployee { get; set; }
+        public SelectList Employees { get; set; }
+    }
+}
