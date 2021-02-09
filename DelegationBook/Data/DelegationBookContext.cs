fac5b5531cc800822d2a46c35d21b5bbcd6e3e8c
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-02-09 10:11:06 +0100

    Add DelegationBookContext class

---
+++ DelegationBook/Data/DelegationBookContext.cs
@@ -0,0 +1,23 @@
+﻿using DelegationBook.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DelegationBook.Data
+{
+    public class DelegationBookContext : DbContext
+    {
+        public DelegationBookContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<KilometersCard> KilometersCards { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+    }
+}
