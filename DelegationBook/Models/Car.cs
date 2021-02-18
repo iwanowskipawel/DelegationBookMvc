f5d4b9b2f335b06efd9231d84512c93d8d02178d
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-02-18 11:11:45 +0100

    Add vaildation rules to Car model

--- DelegationBook/Models/Car.cs
+++ DelegationBook/Models/Car.cs
@@ -10,15 +10,24 @@ namespace DelegationBook.Models
         public int CarId { get; set; }
 
         [Display(Name = "Model")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Nazwa Modelu może mieć maksymalnie 20 znaków ale nie mniej niż 3")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public string Model { get; set; }
 
         [Display(Name = "Numer rejestracyjny")]
+        [StringLength(9, ErrorMessage = "Numer rejestracyjny pojazdu nie może być dłuższy niż 9 znaków")]
+        [RegularExpression(@"[A-Z0-9\s]+", ErrorMessage = "Numer rejestracyjny może zawierać tylko wielkie litery i cyfry")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public string RegistrationNumber { get; set; }
 
         [Display(Name = "Główny kierowca")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public Employee MainDriver { get; set; }
 
         [Display(Name = "Stan licznika")]
+        [DisplayFormat(DataFormatString = "{0:N0} km")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
+        [RegularExpression(@"\d*", ErrorMessage = "Stan licznika należy podać w postaci liczby naturalnej")]
         public int MeterStatus { get; set; }
 
         [Display(Name = "Karty przejechanych km")]
