cb347a2a8fa5fd4a850db3518248e74bb6918856
Paweł Iwanowski <iwanowski3.14@gmail.com>
2021-02-18 08:18:21 +0100

    Add validation rules to Trip and Project models

--- DelegationBook/Models/Project.cs
+++ DelegationBook/Models/Project.cs
@@ -10,12 +10,18 @@ namespace DelegationBook.Models
         public int ProjectId { get; set; }
 
         [Display(Name = "Symbol")]
+        [RegularExpression(@"\d{1}-\d{4}-\d{2}-\d{1}-\d{2}", ErrorMessage ="Symbol tematu musi być w formacie 0-0000-00-0-00")]
+        [Required(ErrorMessage ="Pole obowiązkowe")]
         public string Symbol { get; set; }
 
         [Display(Name = "Zamawiający")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public Company Company { get; set; }
 
         [Display(Name = "Tytuł")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage ="Tytuł tematu musi mieć maksymalnie 100 znaków ale nie mniej niż 3")]
+        [RegularExpression(@"^[A-Z].*", ErrorMessage ="Tytuł poiwnien rozpoczynać się wielką literą")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public string Title { get; set; }
 
         [Display(Name = "Wyjazdy")]
--- DelegationBook/Models/Trip.cs
+++ DelegationBook/Models/Trip.cs
@@ -13,30 +13,43 @@ namespace DelegationBook.Models
 
         [Display(Name = "Data wyjazdu")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd.MM.yyyy} r.")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public DateTime DepartureDate { get; set; }
 
         [Display(Name = "Data powrotu")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy} r.")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public DateTime ArrivalDate { get; set; }
 
         [Display(Name = "Kierowca")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public Employee Driver { get; set; }
 
         [Display(Name = "Temat")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public Project Project { get; set; }
 
         [Display(Name = "Dysponent")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public Employee Keeper { get; set; }
 
         [Display(Name = "Miejsce docelowe")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Nazwa miejsca docelowego musi mieć maksymalnie 50 znaków ale nie mniej niż 3")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public string Destination { get; set; }
 
         [Display(Name = "Początkowy stan licznika")]
         [DisplayFormat(DataFormatString = "{0:N0} km")]
+        [RegularExpression(@"\d*", ErrorMessage = "Stan licznika należy podać w postaci liczby naturalnej")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public int InitialMeter { get; set; }
 
         [Display(Name = "Końcowy stan licznika")]
         [DisplayFormat(DataFormatString = "{0:N0} km")]
+        [RegularExpression(@"\d*", ErrorMessage = "Stan licznika należy podać w postaci liczby naturalnej")]
+        [Required(ErrorMessage = "Pole obowiązkowe")]
         public int FinalMeter { get; set; }
 
         [Display(Name = "Dystans")]
